9e97ce93d04b5649d6c59411225dd2f499919346
funJ <caiquegames67@gmail.com>
2024-04-22 20:36:57 -0300

    commit-tree

--- Project/resources/views/layouts/guest.blade.php
+++ Project/resources/views/layouts/guest.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('css/style_login-register.css') }}">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -17,8 +18,9 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
+                <!-- logo página -->
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{ url('/icon-pinguem') }}" alt="icon-pinguem">
                 </a>
             </div>
 
--- Project/routes/web.php
+++ Project/routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -28,6 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
 
 /* função inserção imgs do carrousel  */
 Route::get('/carrousel-image-1', function () {
@@ -46,9 +47,11 @@ Route::get('/carrousel-image-4', function (){
 // Função iserção de img logo
 
 Route::get('/logo-pinguem', function(){
-    return response()->file(public_path('images/logo_test.png'));
+    return response()->file(public_path('images/logo.png'));
 });
 
+// Função inserção de img icon
 
-
-require __DIR__.'/auth.php';
+Route::get('/icon-pinguem', function (){
+    return response()->file(public_path('images/icon.png'));
+});

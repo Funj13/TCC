6a9e457ad7706fadd89a12e087c4817deb531a9d
funj13 <caiquegames67@gmail.com>
2024-10-13 19:48:27 -0300

    commit-new-topnav

--- Project/resources/views/layouts/main.blade.php
+++ Project/resources/views/layouts/main.blade.php
@@ -14,12 +14,16 @@
   <link rel="stylesheet" href="{{ asset('css/style_css_index.css') }}">
   <!-- <link rel="stylesheet" href="{{ asset('css/style_topnav.css') }}"> -->
 
-  <!-- Google Fonts --> 
+  <!-- Google Fonts -->
   <link rel="stylesheet" href="">
 
   <!-- Boot strap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
 </head>
 
 <body id="body" data-bs-theme="white">
@@ -27,91 +31,78 @@
   @yield('topnav')
   <header>
 
-    <!--Top-Nav-->
-    @auth
 
     <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
-  <div class="bg-dark p-4">
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <form method="POST" action="http://127.0.0.1:8000/logout">
-    @csrf
-  <div class="container-fluid">
-    <a class="navbar-brand" href="http://127.0.0.1:8000/logout" onclick="event.preventDefault();
-                this.closest('form').submit();">Log Out</a>
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" href="/profile">
-          <div>{{ Auth::user()->name }}</div></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/dashboard">Dashboard</a>
-        </li>
-        <li>
-          <!-- layout black and white -->
-          <input type="checkbox" class="theme-checkbox" id="toggle-theme" style="width: 60px;height: 30px;">
-          <script src="js/themeMode.js"></script>
-        </li>
-        </form>
+      <div class="bg-dark p-4">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
+          <div class="container-fluid">
+            @auth
+          <form method="POST" action="http://127.0.0.1:8000/logout">
+            @csrf
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="http://127.0.0.1:8000/logout" onclick="event.preventDefault();
+        this.closest('form').submit();">Log Out</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/profile">
+              <div>{{ Auth::user()->name }}</div>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/dashboard">Dashboard</a>
+            </li>  
+            </ul>
+          </form>
 
       @endauth
-      <!-- Login e registro -->
-      @guest
+            <!-- Login e registro -->
+            @guest
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
           @if (Route::has('login'))
-          <a class="nav-link" href="{{ route('login') }}"> Login</a>
+        <a class="nav-link" href="{{ route('login') }}"> Login</a>
       @endif
-
+          </li>
           @if (Route::has('register'))
-        <a class="nav-link" href="{{ route('register') }}">Cadastra-se</a>
-      @endif
-      <input type="checkbox" class="theme-checkbox" id="toggle-theme">
-          <script src="js/themeMode.js"></script>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="{{ route('register') }}">Cadastra-se</a>
         </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-  </div>
-</div>
-<nav class="navbar navbar-dark bg-dark">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </div>
-</nav>
-      <!--Título-->
-      <div>
-        <h1 style="text-align: center; font-size: 400%; font-style: oblique; "> Pinguim</h1>
+      @endif
+        </ul>
+
+      @endguest
+
+            <!-- layout black and white -->
+            <input type="checkbox" class="theme-checkbox" id="toggle-theme" style="width: 60px;height: 30px;">
+            <script src="js/themeMode.js"></script>
+
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
       </div>
-    @endguest
-    <!--  -->
+      </nav>
+
+    </div>
+    </div>
+
+
+
+
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+          data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
   </header>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+
 
 
   @yield('content')
